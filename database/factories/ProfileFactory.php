2ba066535f94a6a6d094187fdb3530fef9a796dc
haisam <he3@njit.edu>
2019-04-21 23:55:37 -0400

    set up the Feature test for profile, question, answer, and user run the seeder for all

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\pROFILE;
+use Faker\Generator as Faker;
+
+$factory->define(App\PROFILE::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
