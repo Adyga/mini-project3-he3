8c394a2d14ed8ed2967c561a84024577588c2796
haisam <he3@njit.edu>
2019-05-13 21:00:01 -0400

    seeding Category model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UsersTableSeeder::class);
          $this->call(CategoryTableSeeder::class);
-//         $this->call(QuestionTableSeeder::class);
-//         $this->call(AnswerTableSeeder::class);
+         $this->call(QuestionTableSeeder::class);
+         $this->call(AnswerTableSeeder::class);
     }
 }
