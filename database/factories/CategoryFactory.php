ea3c7e9dc91e6dc52572f0e5eecca2b0005aa47c
haisam <he3@njit.edu>
2019-05-13 21:20:16 -0400

    seeding Category model

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,7 +5,7 @@
 use App\Category;
 use Faker\Generator as Faker;
 
-$factory->define(Category::class, function (Faker $faker) {
+$factory->define(App\Category::class, function (Faker $faker) {
     return [
         'name'=>$faker->name,
     ];
